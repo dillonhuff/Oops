fc42c4e74a48e4c2b87f0763bc4d8e5abebf999b
Dillon Huff <dillonhuff@gmail.com>
2015-04-14 21:59:45 -0500

    system test and file manipulation working

---
+++ src/FileManipulation.hs
@@ -0,0 +1,51 @@
+module FileManipulation(
+  applyToFileContents,
+  allFilesWithExtensions,
+  javaExtensions,
+  cExtensions,
+  cppExtensions,
+  getRecursiveContents) where
+
+import Control.Monad (forM, liftM, mapM)
+import Data.List as L
+import System.Directory
+import System.FilePath as Path ((</>))
+import System.FilePath.Posix as Posix
+import System.IO as SIO
+import System.IO.Strict as StrictIO
+
+javaExtensions = [".java"]
+cExtensions = [".c", ".h"]
+cppExtensions = [".cpp", ".h", ".hpp"]
+
+applyToFileContents :: (String -> a) -> (a -> FilePath -> String) -> FilePath -> IO a
+applyToFileContents f log path = do
+  fileH <- SIO.openFile path ReadMode
+  fileContents <- StrictIO.hGetContents fileH
+  let res = f fileContents in
+    do
+      putStr $ log res path
+      return res
+
+allFilesWithExtensions :: [String] -> FilePath -> IO [FilePath]
+allFilesWithExtensions acceptableExtensions dir = do
+  fileNames <- getRecursiveContents dir
+  let sourceFileNames = L.filter (hasAcceptableExtension acceptableExtensions) fileNames in
+    return sourceFileNames
+
+hasAcceptableExtension :: [String] -> FilePath -> Bool
+hasAcceptableExtension acceptableExtensions path = L.elem suffix acceptableExtensions
+  where
+    suffix = Posix.takeExtension path
+
+getRecursiveContents :: FilePath -> IO [FilePath]
+getRecursiveContents topdir = do
+  names <- getDirectoryContents topdir
+  let properNames = filter (`notElem` [".", "..", ".git", ".hg"]) names
+  paths <- forM properNames $ \name -> do
+    let path = topdir </> name
+    isDirectory <- doesDirectoryExist path
+    if isDirectory
+      then getRecursiveContents path
+      else return [path]
+  return (concat paths)
---
+++ test/SystemTest.hs
@@ -0,0 +1,27 @@
+module SystemTest(main) where
+
+import Data.List as L
+import Data.Time.LocalTime
+import Language.Java.Parser
+
+import CheckCompilationUnit
+import FileManipulation
+
+projectDir = "/Users/dillon/clojure"
+
+main :: IO ()
+main = do
+  startTime <- getZonedTime
+  source <- allFilesWithExtensions javaExtensions projectDir
+  res <- mapM (applyToFileContents parseAndCheckCU parseLog) source
+  endTime <- getZonedTime
+  return ()
+
+parseLog :: String -> FilePath -> String
+parseLog res path =
+  "Checked " ++ path ++ " result was:\n" ++ res
+
+parseAndCheckCU str =
+  case parser compilationUnit str of
+    Left err -> "Parse error: " ++ show err ++ "\n"
+    Right compUnit -> (show $ checkCompilationUnit compUnit) ++ "\n"
